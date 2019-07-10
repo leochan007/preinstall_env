eb05138fc1259efd6ef091ffbe77a97ed0464c3c
leo <chan>
2019-07-10 18:46:33 +0800

    update

--- devenv_centos7/scripts/update.sh
+++ devenv_centos7/scripts/update.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
+sudo yum -y install epel-release
 sudo yum -y install centos-release-scl
 sudo yum -y install devtoolset-7-gcc*
 scl enable devtoolset-7 bash
 
-sudo yum -y install python-pip
+sudo yum -y install python-pip telnet lsof
 
 sudo pip install -U pip Cython supervisor numpy -i https://pypi.douban.com/simple/ 
 
